61da843d655804e17f152962708ecbfc21fba8dc
Hagar Fisher <hagarfisher@gmail.com>
2022-03-16 15:44:01 +0200

    Add Typescript support in frontend

---
+++ frontend/supereasy/src/App.tsx
@@ -0,0 +1,11 @@
+import CartButton from "./components/CartButton";
+import ShoppingListInput from "./components/ShoppingListInput";
+
+export default function App() {
+  return (
+    <div>
+      <CartButton />
+      <ShoppingListInput />
+    </div>
+  )
+}
