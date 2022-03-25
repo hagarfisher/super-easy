2b6a7010d84455d7a2552d41f74d9ff3592975a2
Hagar Fisher <hagarfisher@gmail.com>
2022-03-26 00:33:22 +0300

    Add externally_connectable capability

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -4,3 +4,11 @@
 //     files: ['content-script.js']
 //   });
 // });
+chrome.runtime.onMessageExternal.addListener(
+    function (request, sender, sendResponse) {
+        if (request.cartId) {
+            chrome.storage.sync.set({ 'cartId': request.cartId }, function () {
+                console.log('Value is set to ' + request.cartId);
+            });
+        }
+    });
--- chrome_extension/content-script_supereasy.js
+++ chrome_extension/content-script_supereasy.js
@@ -1,7 +1,9 @@
-document.getElementById('createCart').addEventListener('click', () => {
-    const cartId = localStorage.getItem('cartId');
-    console.log(cartId);
-    chrome.storage.sync.set({ 'cartId': cartId }, function () {
-        console.log('Value is set to ' + cartId);
-    });
-});
+// document.getElementById('createCart').addEventListener('click', () => {
+//     setTimeout(() => {
+//         const cartId = localStorage.getItem('cartId');
+//         console.log(cartId);
+//         chrome.storage.sync.set({ 'cartId': cartId }, function () {
+//             console.log('Value is set to ' + cartId);
+//         });
+//     }, 3000);
+// });
