2d0e9899eae733c34c7099025ca12e5403c12c31
Hagar Fisher <hagarfisher@gmail.com>
2022-08-18 21:48:41 +0300

    fixed refresh bug and added spinner

--- frontend/supereasy/src/App.jsx
+++ frontend/supereasy/src/App.jsx
@@ -1,18 +1,50 @@
-import CartButton from "./components/CartButton";
+import axios from "axios";
 import { useState } from "react";
+import CartButton from "./components/CartButton";
 import ShoppingListInput from "./components/ShoppingListInput";
-import "./App.css"
+import { extensionId } from "./utils/constants";
+
+import "./App.css";
+import { Spinner } from "react-bootstrap";
 
 export default function App() {
-  const [isCartExist, setIsCartExist] = useState(localStorage.getItem("cartId") !== "");
-  // const isCartExist = localStorage.getItem("cartId");
-  function toggleCartButton() {
-    setIsCartExist(prevCartId => !prevCartId)
-  }
+  const cartId = localStorage.getItem("cartId");
+  const defaultCartState = !!cartId;
+  const [doesCartExist, setDoesCartExist] = useState(defaultCartState);
+  const [isLoading, setIsLoading] = useState(false);
 
+
+  async function createCart() {
+    const url = "http://localhost:8080/createEmptyCart";
+    try {
+      setIsLoading(true);
+      const response = await axios.get(url);
+      console.log(response);
+      const cartId = response.data.cartId;
+      setDoesCartExist(true);
+      localStorage.setItem("cartId", cartId);
+      window.chrome.runtime.sendMessage(extensionId, { cartId },
+        function (response) {
+          if (!response.success) {
+            console.error(url);
+          }
+        });
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+  if (isLoading) {
+    return <div style={{ width: '100vw', textAlign: 'center', marginTop: '5%' }}><Spinner animation="border" /></div>;
+  }
   return (
     <div className="app">
-      {!isCartExist ? <CartButton /> : <ShoppingListInput />}
+      {doesCartExist ? <ShoppingListInput /> : <CartButton createCart={createCart} />}
+      {/* <button onClick={() => {
+        localStorage.clear();
+        window.location.reload();
+      }}>Clear local storage</button> */}
     </div>
   )
 }
--- frontend/supereasy/src/components/CartButton.jsx
+++ frontend/supereasy/src/components/CartButton.jsx
@@ -1,33 +1,12 @@
-import axios from "axios";
-import { useState } from "react";
 import Button from 'react-bootstrap/Button';
 // import { EmptyCart } from "../types/EmptyCart";i
-import { extensionId } from "../utils/constants";
-export default function CartButton() {
-  // const [cartId, setCartId] = useState("");
-    async function createCart() {
-      const url = "http://localhost:8080/createEmptyCart";
-      try {
-        const response = await axios.get(url);
-        console.log(response);
-        const cartId = response.data.cartId;
-        // setCartId(cartId);
-        localStorage.setItem("cartId", cartId);
-        window.chrome.runtime.sendMessage(extensionId, { cartId },
-          function (response) {
-            if (!response.success) {
-              console.error(url);
-            }
-          });
-      } catch (error) {
-        console.error(error);
-      }
-    }
+
+export default function CartButton({ createCart }) {
 
 
-    return (
-      <div>
-        <Button onClick={(e) => createCart()} id="createCart">Start Shopping</Button>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <Button onClick={(e) => createCart()} id="createCart">Start Shopping</Button>
+    </div>
+  )
+}
